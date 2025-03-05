69ca47f02ef9c350e3a85eddd465e6b80cbdb425
BleynChannel <bleyn2017@gmail.com>
2025-03-05 19:08:16 +0500

    Test update

--- deploy/docker/run-docker-ubuntu.sh
+++ deploy/docker/run-docker-ubuntu.sh
@@ -4,5 +4,7 @@
 
 set -e
 
+BUILD_TYPE=$1
+
 docker build --file ./deploy/docker/Dockerfile-build-ubuntu -t qgc-ubuntu-docker .
-docker run --cap-add SYS_ADMIN --device /dev/fuse --security-opt apparmor:unconfined --rm -v ${PWD}:/project/source -v ${PWD}/build:/project/build qgc-ubuntu-docker
+docker run --cap-add SYS_ADMIN --device /dev/fuse --security-opt apparmor:unconfined --rm -e BUILD_TYPE=${BUILD_TYPE} -v ${PWD}:/project/source -v ${PWD}/build:/project/build qgc-ubuntu-docker
